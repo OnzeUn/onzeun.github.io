310e96c9fdadd1e5e44588532c4847775517fa6d
OnzeUn <118821301+OnzeUn@users.noreply.github.com>
2022-12-13 16:07:50 +0100

    Update exemplelist.js

--- carnetV0/exemplelist.js
+++ carnetV0/exemplelist.js
@@ -34,7 +34,7 @@ function newElement() {
   var t = document.createTextNode(inputValue);
   li.appendChild(t);
   if (inputValue === '') {
-    alert("You must write something!");
+    alert("Vous devez écrire quelque chose !");
   } else {
     document.getElementById("myUL").appendChild(li);
   }
